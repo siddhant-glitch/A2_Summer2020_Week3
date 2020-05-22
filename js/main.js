f8d6ac00fc49b3739f61b4a983eec702750659c5
theMightyT <tvanrys@fanshaweonline.ca>
2020-05-22 12:19:14 -0400

    finished JS pt 2

--- js/main.js
+++ js/main.js
@@ -5,16 +5,27 @@
   console.log('fired!');
 
   // select elements here
-  let theBadge = document.querySelector(".badge"),
-      svgBadge = document.querySelector("svg");
-  // debugger; // pause your code execution here
+  // querySelectorAll is a one-to-many selection -> find EVERY element that matches the selector
+  let theBadges = document.querySelectorAll(".badge"),
+      // one to one connection -> one variable name means one element
+      svgBadge = document.querySelector("svg"),
+      mainHeading = document.querySelector("#mainHeading");
+
+    setTimeout(function() {
+      mainHeading.textContent = "JavaScript really isn't that hard when you understand a few things";
+      mainHeading.classList.add("selected");
+    }, 3000);
+   //debugger; // pause your code execution here
 
   // functions go in the middle
   function logID() {
     console.log("clicked on a graphic - this one: ", this.id);
+    this.classList.add("selected");
   }
 
   // event handling
-  theBadge.addEventListener("click", logID);
+  // theBadge.addEventListener("click", logID);
+  theBadges.forEach(badge => badge.addEventListener("click", logID));
+
   svgBadge.addEventListener("click", logID);
 })();
